05dd5fa9df382ffdd305f3917bde55897df02724
day_worker <aniv42@mail.ru>
2026-02-23 12:29:57 +0700

    second

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -3,11 +3,22 @@ package main
 import "fmt"
 
 func main(){
-	// usd := 100.0
-	const usd_eur = 0.9
-	const usd_rub = 80.0
-	//git remote add origin https://github.com/ТВОЕ_ИМЯ/ИМЯ_РЕПОЗИТОРИЯ.git
-	const eur_rub = usd_rub / usd_eur
-	fmt.Println(eur_rub * 100.0)
+
 	
+}
+
+func inputData() (float64){
+	number := 0.0
+	fmt.Println("Введите сколько валюты готовы обменять: ")
+	fmt.Scan(&number)
+	return number
+}
+
+func exchangeMoney(inputAsset string, inputAssetAmount float64, outputAsset string){
+	// usd := 100.0
+	// const usd_eur = 0.9
+	// const usd_rub = 80.0
+	// //git remote add origin https://github.com/ТВОЕ_ИМЯ/ИМЯ_РЕПОЗИТОРИЯ.git
+	// const eur_rub = usd_rub / usd_eur
+	// fmt.Println(eur_rub * 100.0)
 }
